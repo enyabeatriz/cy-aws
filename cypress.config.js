f3e794357d708987a76f49d5e4c5f03c87943336
enyabeatrizlucena <enyabeatrizlucena@gmail.com>
2024-05-02 18:42:14 -0300

    chore: add required dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
+const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'toc8ge',
+  env:{
+    url: "https://www.amazon.com"
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('file:preprocessor', cucumber())
     },
+    specPattern: "cypress/e2e/*.feature",
+    "chromeWebSecurity": false
+ 
   },
+  defaultCommandTimeout: 8000,
+ 
+
 });
