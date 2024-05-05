0f65c0d0d2ec042426eeb15e7f639d4cf7e8021d
enyabeatrizlucena <enyabeatrizlucena@gmail.com>
2024-05-05 02:18:58 -0300

    chore: add new commands

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,27 +19,57 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-Cypress.Commands.add("getByDataCy",(selector)=>{
+Cypress.Commands.add("getByDataCy", (selector) => {
 
-    return cy.get(`[data-cy=${selector}]`)
+  return cy.get(`[data-cy=${selector}]`)
 })
 
-Cypress.Commands.add("getByDataClass",(selector)=>{
+Cypress.Commands.add("getByDataClass", (selector) => {
 
-    return cy.get(`.${selector}`)
+  return cy.get(`.${selector}`)
 })
 
-Cypress.Commands.add("login", (username, password) => {
+Cypress.Commands.add("login", () => {
+  
+  return cy.fixture('aws_login').then((data) => {
+  cy.session([data.username, data.password], () => {
+    cy.visit(Cypress.env('url'));
+    cy.get('#nav-link-accountList').click()
+    cy.get('#ap_email').click().type(data.username)
+    cy.get('.a-button-inner > #continue').click()
+    cy.get('#ap_password').click().type(data.password)
+    cy.get('#auth-signin-button').click()
+    cy.wait(2000);
+  
+    cy.get('#nav-cart-count').invoke('text').then((text) => {
+      cy.get('#nav-cart').click();
 
-    return cy.session([username, password], () => {
-      cy.visit(Cypress.env('url'));
-      cy.get('#nav-link-accountList').click()
-      cy.get('#ap_email').click().type(username)
-      cy.get('.a-button-inner > #continue').click()
-      cy.get('#ap_password').click().type(password)
-      cy.get('#auth-signin-button').click()
-      cy.wait(2000);
-    }, {
-      cacheAcrossSpecs: true
-    })
+      const itemCount = parseInt(text, 10);
+      if (itemCount > 0) {
+        cy.get('input[data-action="delete"]').first().click({ force: true })
+
+      } else {
+          cy.log('The cart is empty, no items to delete.');
+      }
+    })
+  }, {
+    cacheAcrossSpecs: true
   })
+})})
+
+Cypress.Commands.add("cleanCart", () => {
+  
+  cy.visit(Cypress.env('url'));
+  cy.get('#nav-cart-count').should('be.visible').invoke('text').then((text) => {
+      cy.get('#nav-cart').click();
+
+      const itemCount = parseInt(text, 10);
+      if (itemCount > 0) {
+        cy.get('input[data-action="delete"]').first().click({ force: true })
+
+      } else {
+          cy.log('The cart is empty, no items to delete.');
+      }
+    })
+  
+})
